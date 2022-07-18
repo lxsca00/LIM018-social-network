a78ca52044a0580774cdbf4df2d95ba65e5955b9
Denimarx <denimar2022@gmail.comm>
2022-07-18 09:01:02 -0500

    login

--- src/view/Login.js
+++ src/view/Login.js
@@ -3,17 +3,29 @@
 export default () => {
   const loginView = `
     <div class = 'container-login'>
-    <img id="logo" alt="img popcorn" class= "logo-popcorn" src= "imagenes/pop1.png"   >
-    <form id = "form-login" class="form-login"> 
-    <div>
-     <h1>Inicia Sesión</h1>
-    <input type="email" placeholder="Correo Electrónico" id="ingresaEmail" >
-    <input type="password"  placeholder="Contraseña" id="ingresaContrasena" >
-    <button type="submit" class="btnRegistrar" id="btnRegistrar">¿No tienes cuenta? Regístrate</button>
-    <button type="submit" class= "btnGoogle id="iniciaGoogle">Ingresa con Google</button>
-  </div>
-  </form>
-  </div>`;
+
+      <div class= "container-img-login">
+         <img class="image-login" src="https://i.pinimg.com/originals/54/cc/e0/54cce0449cfd4414fdc19b068a97e00a.png">
+      </div>
+
+      <div class="container-h2-form">
+
+        <div class="h2-login">
+          <h2 class="text-center"> ¡Bienvenidos a PopcornZone! </h2>
+        </div>
+
+        <div class= "container-form-login">
+          <form id = "form-login" class="form-login"> 
+            <h1 class="h1-login">Inicia Sesión</h1>
+            <input type="email" placeholder="Correo Electrónico" id="ingresaEmail" class="input-login-email" >
+            <input type="password"  placeholder="Contraseña" id="ingresaContrasena"class="input-login-password" >
+            <button type="submit" class="btnRegistrar" id="btnRegistrar">¿No tienes cuenta? Regístrate</button>
+            <button type="submit" class= "btnGoogle id="iniciaGoogle">Ingresa con Google</button>
+          </form>
+        </div>
+     </div>
+   </div>`;
+
   const divLogin = document.createElement('div');
   divLogin.id = 'login';
   divLogin.innerHTML = loginView;
