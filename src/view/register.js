98f272541497bc92bb9a9c451e9384ca314c9d6c
lxsca00 <ljimena10@gmail.com>
2022-07-19 11:52:24 -0500

    Credenciales ocultas

--- src/view/register.js
+++ src/view/register.js
@@ -2,17 +2,18 @@
 
 export default () => {
   const viewRegister = `
-  <form id="registerForm">  
-    <input id="userName" type="text" placeholder="Nombre">
-    <input id="userUsername" type="text" placeholder="Nombre de usuario">
-    <input id="userEmail" type="text" placeholder="Email">
-    <input id="userPassword" type="password" placeholder="Password">
-    <input id="userCountry" type="text" placeholder="País">
-    <input id="userBirth" type="date" placeholder="Fecha de nacimiento">
-    <button id="btnRegister" type="submit"> Registrarme </button>
+  <form>  
+    <input type="text" placeholder="Nombre">
+    <input type="text" placeholder="Nombre de usuario">
+    <input type="text" placeholder="Email">
+    <input type="password" placeholder="Password">
+    <input type="text" placeholder="País">
+    <input type="date" placeholder="Fecha de nacimiento">
+    <button type="submit"> Registrarme </button>
   </form>
-  <p> ¿Ya tienes cuenta? </p>
-  <p> <a href="#/login"> Inicia sesión </a> </p>`;
+  <p> O ingresa con: </p>
+  <button> Google </button>
+  <button> Facebook </button>`;
   const divRegister = document.createElement('div');
   divRegister.id = 'register';
   divRegister.innerHTML = viewRegister;
