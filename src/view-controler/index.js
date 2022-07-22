e31a5e510e815242d611974c13cd32a88435eaec
Vanessa <vanessa@MacBook-Air-de-Vanessa.local>
2022-07-22 08:06:04 -0500

    actualizando index-view controler

--- src/view-controler/index.js
+++ src/view-controler/index.js
@@ -1,41 +1,9 @@
-import {
-  eventLogin, eventLogout, eventRegister, sharePost,
-} from '../lib/index.js';
-import { components } from '../view/index.js';
-
-// eslint-disable-next-line consistent-return
-const changeView = (route) => {
-  const container = document.getElementById('container');
-  container.innerHTML = '';
-  switch (route) {
-    case '#/':
-    { return container.appendChild(components.login()); }
-    case '#/principal':
-    { container.appendChild(components.home());
-      return sharePost(); }
-    case '#/registro':
-      // eslint-disable-next-line no-lone-blocks
-      { container.appendChild(components.register());
-        eventRegister(); }
-      break;
-    case '#/login':
-    { container.appendChild(components.login());
-      return eventLogin(); }
-    case '#/cerrarSesion':
-    { return eventLogout(); }
-    default:
-      break;
-  }
-  // console.log(route);
-};
-export { changeView };
-
-// RAMA VANESSA
 import { components } from '../view/index.js';
 import {
-  eventRegister,
-  sharePost,
-  eventLogin2,
+  fEventRegister,
+  fEventLogin,
+  fEventLogout,
+  fSharePost,
 } from '../main.js';
 
 // eslint-disable-next-line consistent-return
@@ -47,20 +15,23 @@ const changeView = (route) => {
     { return container.appendChild(components.login()); }
     case '#/principal':
     { container.appendChild(components.home());
-      return sharePost(); }
+      return fSharePost(); }
     case '#/registro':
       // eslint-disable-next-line no-lone-blocks
       { container.appendChild(components.register());
-        eventRegister(); }
+        fEventRegister(); }
       break;
     case '#/login':
     { container.appendChild(components.login());
-      return eventLogin2(); }
+      return fEventLogin(); }
+    case '#/cerrarSesion':
+    { return fEventLogout(); }
     default:
       break;
   }
   // console.log(route);
 };
+// export { changeView };
 
 // PARA CAMBIAR DE VISTA
 const init = () => {
