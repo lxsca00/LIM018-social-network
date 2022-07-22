31a1f7fb63af4ca1620d8ea9fefd4228ccaa6c48
Vanessa <vanessa@MacBook-Air-de-Vanessa.local>
2022-07-22 06:26:52 -0500

    uniendo con rama vanessa test

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,3 +6,37 @@ describe('myFunction', () => {
     expect(typeof myFunction).toBe('function');
   });
 });
+
+// RAMA VANESSA
+import {
+  ingreso,
+  
+} from '../src/lib/index.js';
+
+describe('login (ingreso)', () => {
+  it('debería ser una función', () => {
+    expect(typeof ingreso).toBe('function');
+  });
+  // https://jestjs.io/es-ES/docs/asynchronous
+  // RESOLVES
+  test('login (ingreso) para usuario registrado', () => expect(ingreso('sss@gmail.com', '123456')).resolves.toBe('El sss@gmail.com si tiene una cuenta activa'));
+  test('login (ingreso) para usuario sin registrar', () => expect(ingreso('Ab1wqwed23@gmail.com', 'aqwe456')).resolves.toBe('Firebase: Error (auth/user-not-found).'));
+  // https://jestjs.io/es-ES/docs/asynchronous
+  // ASYNC/AWAIT
+  //   test('login (ingreso) para usuario registrado', async () => {
+//     const login = await ingreso('sss@gmail.com', '123456');
+//     expect(login).toBe('El sss@gmail.com si tiene una cuenta activa');
+//   });
+});
+
+describe('register', () => {
+  it('debería ser una función', () => {
+    expect(typeof ingreso).toBe('function');
+  });
+  // https://jestjs.io/es-ES/docs/asynchronous
+  // ASYNC/AWAIT
+  //   test('login (ingreso) para usuario registrado', async () => {
+//     const login = await ingreso('sss@gmail.com', '123456');
+//     expect(login).toBe('El sss@gmail.com si tiene una cuenta activa');
+//   });
+});
