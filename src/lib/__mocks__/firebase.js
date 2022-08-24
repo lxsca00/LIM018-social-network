0fe5101dc1c86ca24c3edcd65aff55a1ea4cc845
Vanessa <vanessaah.lu@gmail.com>
2022-08-24 00:22:11 -0500

    Mocking cambiosVane match main_last

--- src/lib/__mocks__/firebase.js
+++ src/lib/__mocks__/firebase.js
@@ -5,6 +5,8 @@ export const getAuth = jest.fn();
 export const auth = { currentUser: { uid: '123er' } };
 // export const auth = { currentUser: null };
 
+// export const auth = jest.fn({ currentUser: { uid: '123er' } });
+
 export const getFirestore = jest.fn();
 export const collection = jest.fn();
 // export const GoogleAuthProvider = jest.fn();
@@ -66,6 +68,42 @@ export const GoogleAuthProvider = jest.fn(() => ({ credentialFromResult: () => P
 // export const onAuthStateChanged = (auth, jest.fn(() => Promise.resolve({ user: { uid: '123er' } })));
 export const onAuthStateChanged = (auth, jest.fn(() => Promise.resolve()));
 
-/* **************** home ************************ */
+// export const onAuthStateChanged = () => jest.fn();
 
+// export const onAuthStateChanged = (jest.fn(() => Promise.resolve({ user: { xxx: 'cc' } })));
+// eslint-disable-next-line max-len
+// export const onAuthStateChanged = (auth, jest.fn((user) => { if (user) { Promise.resolve({ user: { xxx: 'cc' } }); } }));
+
+// eslint-disable-next-line no-unused-vars
+// export const onAuthStateChanged = jest.fn((auth, callback) => Promise.resolve({ user: {} }));
+// export const onAuthStateChanged = (auth, (user) => { if (user) { console.log(user); } });
+
+/* **************** home ************************ */
+// export const uid = jest.fn({ uid: 'dd' });
+// export const currentUser = jest.fn({ uid });
+// export const auth = { currentUser: { uid: 'ddd' } };
+export const getDoc = jest.fn();
+// export const doc = jest.fn();
 export const Timestamp = { fromDate: (date) => date };
+// setDoc
+// export const addDoc = jest.fn(() => {
+//   Promise.resolve({
+//     birth: 'birth',
+//     ountry: 'country',
+//     description: 'description',
+//     email: 'email',
+//     password: 'password',
+//     name: 'name',
+//     username: 'username',
+//     uid: 'uid',
+//     photo: 'photo',
+//   });
+// });
+// export const doc = {
+//   country: 'country',
+//   description: 'description',
+//   email: 'email',
+//   password: 'password',
+//   uid: 'ddd',
+//   photo: 'https://cdn-icons-png.flaticon.com/512/4222/4222009.png',
+// };
--- src/view-controler/index.js
+++ src/view-controler/index.js
@@ -51,41 +51,4 @@ const changeView = (route) => {
   }
 };
 
-
-// CERRAR SESIÓN
-export const fEventLogout = () => {
-  const logout = document.querySelector('#logout');
-  logout.addEventListener('click', (e) => {
-    e.preventDefault();
-    eventLogout();
-  });
-};
-
-// FUNCION PARA COMPARTIR UN POST EN HOME
-export function fSharePost() {
-  const toShare = document.getElementById('toShare');
-  let numberPost = 0;
-  toShare.addEventListener('click', () => {
-    const oldPost = `
-      <div class="old-publication" >
-        <p class="user-name-post">AQUI VA EL NOMBRE DE USUARIO</p>
-        <input type="text" class="old-comment">
-        <div class="container-button">
-          <div class="emojis">
-            <input type="button" title="Click to coment" value="🍿"  class="button-emoji" >
-            <input type="button" title="Click to coment" value="🤍"  class="button-emoji" >
-          </div>
-         <input type="button" title="Click to coment" value="Comentar "  class="comment-button" >
-        </div>
-          </div>
-      </div>`;
-
-    const parentPost = document.getElementById('all-publications');
-    const divElem = document.createElement('div');
-    // se debe almacenar en un solo div porque sino "to Node.appendChild must be an instance of Nod"
-    numberPost += 1;
-    divElem.id = `post ${numberPost}`;
-    divElem.innerHTML = oldPost;
-    return parentPost.appendChild(divElem);
-  });
-}
+export { changeView };
